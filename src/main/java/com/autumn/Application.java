cfa61e02d44063c8f54fa503440f16cb9fdf4b42
autumn <1060590974@qq.com>
2024-01-17 17:45:20 +0800

    初始化

---
+++ src/main/java/com/autumn/Application.java
@@ -0,0 +1,19 @@
+package com.autumn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ *
+ * @author autumn
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class Application {
+    public static void main(String[] args) {
+        // System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(Application.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  启动成功   ლ(´ڡ`ლ)ﾞ  \n");
+    }
+}
